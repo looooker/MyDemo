afad2a452fe52e97a66e42bcffd345d1336a11ba
master <looooker@163.com>
2015-10-16 11:00:42 +0800

    我是一只小鸭子

--- app/src/main/java/com/example/master/mydemo/MainActivity.java
+++ app/src/main/java/com/example/master/mydemo/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_CoordinatorLayout).setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_CoordinatorLayout:
+
+                CoordinatorLayoutActivity.actionStart(this);
+
+                break;
+            default:
+                break;
+        }
     }
 }
